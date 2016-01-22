005702801fb4efae28dec22fa51fdfd029571381
Dobromir Brezoev <dobromir.brezoev@amexis.net>
2016-01-22 15:07:59 +0200

    copy css tasks

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -48,10 +48,34 @@ gulp.task('build-system', ['build-html-system'], function () {
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
+gulp.task('copy-css-es6', function() {
+  return gulp.src(paths.aureliaDialogCss)
+    .pipe(gulp.dest(paths.output + 'es6/aurelia-dialog-wrapper'));
+});
+
+gulp.task('copy-css-commonjs', function() {
+  return gulp.src(paths.aureliaDialogCss)
+    .pipe(gulp.dest(paths.output + 'commonjs/aurelia-dialog-wrapper'));
+});
+
+gulp.task('copy-css-amd', function() {
+  return gulp.src(paths.aureliaDialogCss)
+    .pipe(gulp.dest(paths.output + 'amd/aurelia-dialog-wrapper'));
+});
+
+gulp.task('copy-css-system', function() {
+  return gulp.src(paths.aureliaDialogCss)
+    .pipe(gulp.dest(paths.output + 'system/aurelia-dialog-wrapper'));
+});
+
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
     ['build-es6', 'build-commonjs', 'build-amd', 'build-system'],
+    'copy-css-commonjs',
+    'copy-css-amd'
+    'copy-css-es6'
+    'copy-css-system'
     callback
   );
 });
